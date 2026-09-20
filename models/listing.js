1f9644e09c69a8ca3b35eab23b06509f59591f30
Pranav Rahangdale <pranavrahangdale888@gmail.com>
2026-09-20 18:49:41 +0530

    Improve listing UI and forms

--- models/listing.js
+++ models/listing.js
@@ -10,15 +10,20 @@ const listingSchema = new Schema({
     description: String,
 
     image: {
-        filename: {
-            type: String,
-            default: "listingimage"
-        },
-        url: {
-            type: String,
-            default: "https://images.unsplash.com/photo-1773332585749-5146862ba746?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    type: String,
+    default: "https://images.unsplash.com/photo-1773332585749-5146862ba746?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set: (v) => {
+        if (typeof v === "object") {
+            return v.url;
         }
-    },
+
+        if (v === "") {
+            return "https://images.unsplash.com/photo-1773332585749-5146862ba746?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+        }
+
+        return v;
+    }
+},
 
     price: Number,
     location: String,
